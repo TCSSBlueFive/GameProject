e3c2518be52645b7a8cc43fda5a8fef14e07cde6
DavidPerez97 <38087886+DavidPerez97@users.noreply.github.com>
2019-01-28 17:28:52 -0800

    changed idle back to normal

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var AM = new AssetManager();
 AM.queueDownload("./img/slime_sprite.png");
 AM.queueDownload("./img/background2.jpg");
 AM.queueDownload("./img/button.png")
-AM.queueDownload("./img/idle blink 2.png");
+AM.queueDownload("./img/idle blink.png");
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
     var ctx = canvas.getContext("2d");
@@ -17,7 +17,7 @@ AM.downloadAll(function () {
     gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/background2.jpg")));
     gameEngine.addEntity(new Enemy(gameEngine, AM.getAsset("./img/slime_sprite.png")));
 
-    gameEngine.addEntity(new PlayerCharacter(gameEngine, AM.getAsset("./img/idle blink 2.png")));
+    gameEngine.addEntity(new PlayerCharacter(gameEngine, AM.getAsset("./img/idle blink.png")));
     gameEngine.addEntity(new Button(gameEngine, AM.getAsset("./img/button.png")));
 
 
