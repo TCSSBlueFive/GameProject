19e62cbea29dacc6b38d372f4807e84b09928c5a
TCSSBlueFive <46801681+TCSSBlueFive@users.noreply.github.com>
2019-02-01 19:30:19 -0800

    Update EnemyDataBase.js

--- EnemyDataBase.js
+++ EnemyDataBase.js
@@ -1,5 +1,5 @@
 //Level 1 Slime
-function EnemyDatabase(game, spritesheet) {
+function EnemyDataBase(game, spritesheet) {
     this.monsters = [{health: 100, name: 'slime', attacks: 10, 
     animation: new Animation(spritesheet, 256, 256, 1, .075, 18, true, 0.5, 1),
     attackAnimation: new Animation(spritesheet, 256, 256, 1, .075, 18, true, 0.5, 2)},
