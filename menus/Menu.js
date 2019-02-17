f2d12753b906a0e3798b83bf0901d37892d17bdf
Sam Stone <sstone@redquarry.com>
2019-02-17 10:54:43 -0800

    main menu

---
+++ menus/Menu.js
@@ -0,0 +1,7 @@
+function returnToMainMenu() {
+  //<canvas id="gameWorld" tabindex="1" width="1400" height="700" style="width: 100%; border: 1px solid brown; background: lightskyblue"></canvas>
+}
+
+function startNewGame() {
+  document.location.href = '../../canvas.html'
+}
