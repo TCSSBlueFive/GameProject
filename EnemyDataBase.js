f1e83ef14b57d7c8ff3fbd924a555d0a76a6827d
samhip <samhipolito@gmail.com>
2019-02-20 22:57:43 -0800

    adjusted succubus spritesheet/animations

--- EnemyDataBase.js
+++ EnemyDataBase.js
@@ -23,9 +23,9 @@ function EnemyDataBase(game, spritesheet) {
             type: 'damage',
             value: 90
         },
-        animation: new Animation(AM.getAsset("./img/succubus_basic.png"), 100, 141, 1, 0.10, 7, true, 1, 1,0),
-        attackAnimation: new Animation(AM.getAsset("./img/succubus_basic.png"), 200, 150, 1, 0.15, 6, false, 1, 2,0),
-        damagedAnimation: new Animation(AM.getAsset("./img/succubus_basic.png"), 100, 140, 1, 0.15, 4, false, 1, 0,0),
-        deathAnimation: new Animation(AM.getAsset("./img/succubus_basic.png"), 100, 140, 1, 0.15, 7, true, 1, 0,0)
+        animation: new Animation(AM.getAsset("./img/succubus_basic.png"), 200, 190, 1, 0.10, 7, true, 1, 1,0),
+        attackAnimation: new Animation(AM.getAsset("./img/succubus_basic.png"), 200, 190, 1, 0.15, 6, false, 1, 2,0),
+        damagedAnimation: new Animation(AM.getAsset("./img/succubus_basic.png"), 200, 190, 1, 0.15, 4, false, 1, 0,0),
+        deathAnimation: new Animation(AM.getAsset("./img/succubus_basic.png"), 200, 190, 1, 0.15, 7, false, 1, 0,0)
     }]
 };
